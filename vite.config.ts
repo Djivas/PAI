95bb59b22bd277607546da2f15d6117052652be6
Djivas <145970973+Djivas@users.noreply.github.com>
2023-10-16 16:16:00 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // Disable WebSocket for HMR
+    hmr: {
+      protocol: 'ws' // Set this to null or an empty string to disable WebSocket
+    }
+  }
 })
